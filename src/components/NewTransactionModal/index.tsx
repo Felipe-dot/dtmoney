24c2964761146c49a264f621c4427c35f7c1424a
Luis Felipe de Azevedo Melo <felipeluis610@gmail.com>
2022-05-27 21:05:19 -0300

    Finalizing the form style

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -2,7 +2,8 @@ import Modal, { Props } from "react-modal";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import closeImg from "../../assets/close.svg";
-import {Container, TransactionTypeContainer} from "./styles";
+import {Container, RadioBox, TransactionTypeContainer} from "./styles";
+import { useState } from "react";
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -10,6 +11,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: Props) {
+    const [ type, setType] = useState('deposit');
+    
     return(
         <Modal 
         isOpen={isOpen}
@@ -36,18 +39,25 @@ export function NewTransactionModal({isOpen, onRequestClose}: Props) {
                 />
 
                 <TransactionTypeContainer>
-                    <button
-                        type="button"
+                    <RadioBox
+                         type="button"
+                         onClick={() => setType('deposit')}
+                         isActive={type === 'deposit'}
+                         activeColor="green"
                     >
                         <img src={incomeImg} alt="Entrada" />
                         <span>Entrada</span>
-                    </button>
-                    <button
+                    </RadioBox>
+        
+                    <RadioBox
                         type="button"
+                        onClick={() => setType('withdraw')}
+                        isActive={type === 'withdraw'}
+                        activeColor="red"
                     >
                         <img src={outcomeImg} alt="Saída" />
                         <span>Saída</span>
-                    </button>
+                    </RadioBox>
                 </TransactionTypeContainer>
 
                 <input 
